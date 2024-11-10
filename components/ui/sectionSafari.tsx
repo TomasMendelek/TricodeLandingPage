77810c7521757ae997e5c06ec24e812ed3fcf832
TomasMendelek <tomascmendelek@gmail.com>
2024-11-10 17:29:01 -0300

    add Components UI

---
+++ components/ui/sectionSafari.tsx
@@ -0,0 +1,13 @@
+import Safari from "./safari";
+
+export function SectionSafari() {
+  return (
+    <div className="relative m-auto py-3 max-w-5xl">
+      <Safari
+        url="tricodeadmin.com"
+        className="size-full object-cover"
+        src="/images/tricodeadmin.png"
+      />
+    </div>
+  );
+}
